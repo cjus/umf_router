e681510602418470f5dfa8b768e801592683a70d
Carlos Justiniano <carlos.justiniano@gmail.com>
2013-11-04 10:06:27 -0800

    Added weak UUID generator for use with creating unique message IDs.

---
+++ app/static/vendor/weak-uuid.js
@@ -0,0 +1,13 @@
+// based on: http://jsfiddle.net/briguy37/2MVFd/
+var UUID = {
+  'generateWeakUUID': function () {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+    });
+    return uuid;
+  }
+};  // UUID namespace
+
