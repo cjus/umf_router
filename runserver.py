e62a5be9f1cb8722621de4a9fcc9ea28414d8aa9
Carlos Justiniano <carlos.justiniano@gmail.com>
2013-11-11 15:57:04 -0800

    Added gevent monkey patch support, although it isn't necessary for this demo. It would be useful in larger projects.

--- runserver.py
+++ runserver.py
@@ -2,12 +2,15 @@
 """Init WSGIServer and websocket_handler"""
 __author__ = 'carlosjustiniano'
 
+from gevent import monkey
 from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 
 from app import websocket_handler
 
 if __name__ == '__main__':
+    monkey.patch_all()
     WEB_SOCKET_PORT = 80
-    http_server = WSGIServer(('', WEB_SOCKET_PORT), websocket_handler, handler_class=WebSocketHandler)
+    http_server = WSGIServer(('', WEB_SOCKET_PORT), websocket_handler,
+                             handler_class=WebSocketHandler)
     http_server.serve_forever()
