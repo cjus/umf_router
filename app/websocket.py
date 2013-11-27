0d4458bd9c7da45b1d9bcfcd09a6ba7ac400e022
Carlos Justiniano <carlos.justiniano@gmail.com>
2013-11-27 14:32:10 -0800

    Added mid and rmid message types

--- app/websocket.py
+++ app/websocket.py
@@ -15,7 +15,29 @@ from umf.umf_router import UMFRouter
 from umf.umf_message import UMFMessageField
 
 
+def send_message(ws, message_fragment):
+    """utility function to simplify sending messages"""
+    t = time.gmtime()
+    time_stamp = "%d/%2.2d/%2.2dT%2.2d:%2.2d:%2.2dZ" % \
+                 (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour,
+                  t.tm_min, t.tm_sec)
+    msg = {
+        "mid": uuid4().hex,
+        "type": "msg",
+        "to": "",
+        "from": "umfTestServer",
+        "version": "1.0",
+        "timestamp": time_stamp,
+        "body": {
+        }
+    }
+    # merge message_fragment into msg updating supplied message fields
+    msg = dict(msg.items() + message_fragment.items())
+    ws.send(json.dumps(msg))
+
+
 def handle_websocket(ws):
+    """Main socket handler"""
     while True:
         ws_message = ws.receive()
         if ws_message is None:
@@ -24,21 +46,21 @@ def handle_websocket(ws):
         else:
             msg_dict = json.loads(ws_message)
             umf_router = UMFRouter()
-            umf_router.route(msg_dict)
-
-            if random.randint(0, 10) > 5:
-                t = time.gmtime()
-                time_stamp = "%d/%2.2d/%2.2dT%2.2d:%2.2d:%2.2dZ" % \
-                             (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour,
-                              t.tm_min, t.tm_sec)
-                msg = {
-                    "mid": uuid4().hex,
-                    "type": "chat",
+            routed = umf_router.route(msg_dict)
+            if not routed:
+                send_message(ws, {
+                    "type": "error",
+                    "rmid": msg_dict[UMFMessageField.MID],
                     "to": msg_dict[UMFMessageField.FROM],
-                    "from": "umfTestServer",
-                    "version": "1.0",
-                    "timestamp": time_stamp,
                     "body": {
+                        "errorcode": 400,
+                        "message": "unable to route message."
                     }
-                }
-                ws.send(json.dumps(msg))
+                })
+                # sample code to randomly send a message back to client
+            if random.randint(0, 10) > 5:
+                send_message(ws, {
+                    "type": random.choice(['chat', 'heart',
+                                           'mouse', 'client']),
+                    "to": msg_dict[UMFMessageField.FROM]
+                })
